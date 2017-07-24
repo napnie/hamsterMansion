8590e4c56779ee02c14271777ed561a05e523a55
Nitith Chayakul <c.nitith@gmail.com>
2017-07-24 09:10:06 +0700

    make hamster status changeable

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-    
+    var link = "http://158.108.165.223/data/OACEED/"
+
+    $('#hamStatus').click(function() {
+        var $hamster = $(this)
+        if($hamster.text() == "Yes") $hamster.text("No")
+        else $hamster.text("Yes")
+    })
+
+    function receive(variable, anwer) {
+
+    }
+
+    setInterval(function() {
+        
+    }, 5000)
 })

c80abee5a5b8a0a1e5d2c3c69ff0b144b06ca598
Nitith Chayakul <c.nitith@gmail.com>
2017-07-24 09:44:31 +0700

    make lure function

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var link = "http://158.108.165.223/data/OACEED/"
 
-    $('#hamStatus').click(function() {
+    function send(value, variable = "") {
+        console.log(link + variable + (variable ? "/" : "") + "set/" + value)
+        $.ajax({
+            url: link + variable + (variable ? "/" : "") + "set/" + value
+        }).done(function () {
+
+        })
+    }
+
+    var howHam = function () {
+        var $hamster = $('#hamStatus')
+        $.ajax({
+            url: link + "deathORalive"
+        }).done(function (data) {
+            console.log(data)
+            if (data == 1) {
+                $hamster.text("Yes")
+            }
+            else {
+                $hamster.text("No")
+            }
+        })
+    }
+
+    $('#hamStatus').click(function () {
         var $hamster = $(this)
-        if($hamster.text() == "Yes") $hamster.text("No")
+        if ($hamster.text() == "Yes") $hamster.text("No")
         else $hamster.text("Yes")
     })
 
-    function receive(variable, anwer) {
+    $('#lure').click(function () {
+        send(1, "Lure")
+        while ($hamster.text() != "Yes") {
+            await(3000)
+            howHam()
+        }
+        send(0, "Lure")
+    })
 
-    }
-
-    setInterval(function() {
-        
+    setInterval(function () {
+        var hamStatus
+        howHam()
     }, 5000)
 })
